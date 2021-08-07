8d05de5278fe7e42f9f152e531e70ad82fa251b1
Draxxi <36058069+Draxxi@users.noreply.github.com>
2021-08-07 14:49:01 -0700

    Create Music.cs
    Music Handler (Don't Destroy on new level)

---
+++ Milky-Way-Dream/Music.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
