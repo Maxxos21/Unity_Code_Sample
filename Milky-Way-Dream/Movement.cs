34f43abb40cfd1595e3f85928f28d1821e635832
Draxxi <36058069+Draxxi@users.noreply.github.com>
2021-08-07 14:48:27 -0700

    Create Movement.cs
    movement for rocket

---
+++ Milky-Way-Dream/Movement.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //cached reference
+    Rigidbody myRigidBody;
+    AudioSource myAudioSource;
+
+    //config 
+    [SerializeField] float mainThrust = 5f;
+    [SerializeField] float sideThrust = 20f;
+    [SerializeField] AudioClip mainEngine;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidBody.AddRelativeForce(Vector3.up * Time.deltaTime * mainThrust);
+            if(!myAudioSource.isPlaying)
+            {
+                PlaySFX();
+            }
+            if (!mainEngineParticles.isPlaying)
+            {
+                mainEngineParticles.Play();
+            }
+            
+        }
+        else
+        {
+            myAudioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+
+
+    }
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApllyThrust(-sideThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApllyThrust(sideThrust);
+        }
+    }
+
+    private void ApllyThrust(float rotationThisFrame)
+    {
+        myRigidBody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+        myRigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
+    }
+
+    private void PlaySFX()
+    {
+        myAudioSource.PlayOneShot(mainEngine);
+    }
+}
+
+    
