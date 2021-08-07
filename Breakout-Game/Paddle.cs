6610cc13567be469906189afdb945efea9a84794
Draxxi <36058069+Draxxi@users.noreply.github.com>
2021-08-06 17:19:57 -0700

    Create Paddle.cs
    Script for any paddle in the game

---
+++ Breakout-Game/Paddle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    // configuration  paramters
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    //cached reff
+
+    GameStatus theGameStatus;
+    Ball theBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theGameStatus = FindObjectOfType<GameStatus>();
+        theBall = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameStatus.IsAutoPlayEnable())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
